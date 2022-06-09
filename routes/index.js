f9fc36e54105743947aef8965d7e66b397da2eca
Sravan <sravankomati.vision@gmail.com>
2022-06-09 18:17:13 +0530

    dcdc

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,9 @@ const passport = require("passport");
 const index = express.Router();
 const auth = passport.authenticate("jwt", { session: false });
 index.get("/user/verifyemail/:token", userCont.verifyEmail);
-<<<<<<< HEAD
 index.use("/user", userMiddleware.tokenVerify, userRout);
 index.use("/movie", [auth, userMiddleware.tokenVerify], movieRout);
-=======
 index.use("/user", auth.tokenVerify, userRout);
 index.use("/movie", auth.tokenVerify, movieRout);
->>>>>>> 23c0a932ccaf2d4f5a32eccceab7fe13e93a13b7
 
 module.exports = index;
